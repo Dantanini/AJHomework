f981d968a1b3f3611793ff3cec236017689f38fb
Dante <dante@sixnology.com>
2020-05-17 20:25:09 +0800

    Remove unused cache code

--- app/src/main/java/com/dantanini/ajhomework/api/WeatherApiService.kt
+++ app/src/main/java/com/dantanini/ajhomework/api/WeatherApiService.kt
@@ -2,13 +2,11 @@ package com.dantanini.ajhomework.api
 
 import com.dantanini.ajhomework.model.WeatherResponse
 import retrofit2.http.GET
-import retrofit2.http.Header
 import retrofit2.http.Query
 
 interface WeatherApiService {
     @GET("v1/rest/datastore/F-C0032-001")
     suspend fun getWeatherResponse(
-        @Header("cache-control") refresh: Boolean,
         @Query("Authorization") apiKey: String,
         @Query("locationName") locationName: String,
         @Query("elementName") elementName: String,
--- app/src/main/java/com/dantanini/ajhomework/api/WeatherManager.kt
+++ app/src/main/java/com/dantanini/ajhomework/api/WeatherManager.kt
@@ -1,27 +1,21 @@
 package com.dantanini.ajhomework.api
 
-import android.content.Context
 import com.dantanini.ajhomework.model.Weather
-import okhttp3.Cache
-import okhttp3.Interceptor
 import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
-import java.lang.Exception
 
-private const val CACHE_SIZE = (5 * 1024 * 1024).toLong() //5MB
-private const val CACHE_TIME_SECOND = 60 * 60 * 24
-private const val CACHE_ONLY_KEYWORD = "only-if-cached"
 private const val LOCATION_TAIPEI = "臺北市"
 private const val ELEMENT_NAME = "MinT"
 private const val FORMAT_JSON = "JSON"
 
-class WeatherManager private constructor(private val context: Context, private val apiKey: String) {
+class WeatherManager(private val apiKey: String) {
     private var apiService: WeatherApiService
 
     init {
         val retrofit = Retrofit.Builder()
-            .baseUrl("opendata.cwb.gov.tw/api/")
+            .baseUrl("https://opendata.cwb.gov.tw/api/")
             .addConverterFactory(GsonConverterFactory.create())
             .client(getApiClient())
             .build()
@@ -29,30 +23,17 @@ class WeatherManager private constructor(private val context: Context, private v
     }
 
     private fun getApiClient(): OkHttpClient {
-        val cache = Cache(context.cacheDir, CACHE_SIZE)
-
-        val interceptor = Interceptor { chain: Interceptor.Chain ->
-            val request = chain.request()
-            val refresh = request.cacheControl.toString() == true.toString()
-            val cacheInfo =
-                if (refresh) "no-cache" else "public, $CACHE_ONLY_KEYWORD, max-stale=$CACHE_TIME_SECOND"
-            val newRequest = request.newBuilder()
-                .removeHeader("cache-control")
-                .addHeader("cache-control", cacheInfo)
-                .build()
-            chain.proceed(newRequest)
-        }
+        val loggingInterceptor = HttpLoggingInterceptor()
+        loggingInterceptor.level = HttpLoggingInterceptor.Level.BODY
 
         return OkHttpClient.Builder()
-            .cache(cache)
-            .addInterceptor(interceptor)
+            .addInterceptor(loggingInterceptor)
             .retryOnConnectionFailure(true)
             .build()
     }
 
-    suspend fun getTaipeiWeathers(refresh: Boolean): List<Weather> {
-        val weatherResponse =
-            apiService.getWeatherResponse(refresh, apiKey, LOCATION_TAIPEI, ELEMENT_NAME, FORMAT_JSON)
+    suspend fun getTaipeiWeathers(): List<Weather> {
+        val weatherResponse = apiService.getWeatherResponse(apiKey, LOCATION_TAIPEI, ELEMENT_NAME, FORMAT_JSON)
 
         if (weatherResponse.success) {
             return weatherResponse.records.location[0].weatherElement[0].time
