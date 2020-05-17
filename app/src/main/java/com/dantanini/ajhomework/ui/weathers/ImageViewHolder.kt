6e6bd96235ee9ecc734134df86d41cac6d65de36
Dante <dante@sixnology.com>
2020-05-17 17:46:48 +0800

    Add ImageViewHolder

---
+++ app/src/main/java/com/dantanini/ajhomework/ui/weathers/ImageViewHolder.kt
@@ -0,0 +1,6 @@
+package com.dantanini.ajhomework.ui.weathers
+
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+
+class ImageViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
