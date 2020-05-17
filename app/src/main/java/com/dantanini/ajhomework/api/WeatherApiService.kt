06b002d2d95185202cf43b92227210a5fceb47a2
Dante <dante@sixnology.com>
2020-05-17 16:07:29 +0800

    Add WeatherApiService

---
+++ app/src/main/java/com/dantanini/ajhomework/api/WeatherApiService.kt
@@ -0,0 +1,15 @@
+package com.dantanini.ajhomework.api
+
+import com.dantanini.ajhomework.model.WeatherResponse
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface WeatherApiService {
+    @GET("v1/rest/datastore/F-C0032-001")
+    suspend fun getWeather(
+        @Query("Authorization") apiKey: String,
+        @Query("locationName") locationName: String,
+        @Query("elementName") elementName: String,
+        @Query("format") format: String
+    ): WeatherResponse
+}
