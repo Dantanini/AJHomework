7be9a3da7b28e0b5052a1b3e1ba15d03187e58f2
Dante <dante@sixnology.com>
2020-05-17 22:14:27 +0800

    Add MyConstants

---
+++ app/src/main/java/com/dantanini/ajhomework/ui/MyConstants.kt
@@ -0,0 +1,9 @@
+package com.dantanini.ajhomework.ui
+
+class MyConstants {
+    companion object {
+        const val START_TIME = "startTime"
+        const val END_TIME = "endTime"
+        const val TEMPERATURE = "temperature"
+    }
+}
