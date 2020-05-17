de16537b085d23fc264f0ffa97f2f018c084b95b
Dante <dante@sixnology.com>
2020-05-17 17:13:04 +0800

    Add temperature in Weather

--- app/src/main/java/com/dantanini/ajhomework/model/Weather.kt
+++ app/src/main/java/com/dantanini/ajhomework/model/Weather.kt
@@ -1,7 +1,5 @@
 package com.dantanini.ajhomework.model
 
-data class Weather(
-    val endTime: String,
-    val parameter: Parameter,
-    val startTime: String
-)
+data class Weather(val endTime: String, val parameter: Parameter, val startTime: String) {
+    val temperature get() = parameter.parameterName + parameter.parameterUnit
+}
